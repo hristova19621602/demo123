1c2eff416c875b6375d6849cb800b1f4e33c115d
TaniaBorislav <92330815+hristova19621602@users.noreply.github.com>
2022-03-12 14:07:41 +0200

    rrrr3

--- src/main/java/modules/modul.java
+++ src/main/java/modules/modul.java
@@ -2,8 +2,29 @@ package modules;
 
 public class modul {
     private String name;
+    private int age;
 
     public modul(String name) {
         this.name = name;
+    }
+
+    public modul(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
     }
 }
